4eb38de2593246950786de4f39bfcea987f3524d
Petr Boháč <bohac3@spsejecna.cz>
2023-11-10 08:07:00 +0100

    idk isc

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Find the "Nabídka" link element
     const navLink = document.querySelector('a[aria-current="page"][href="#"]');
-    
     const itemListSection = document.querySelector('#header-item-list');
 
     navLink.addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent the default link behavior
-
-        // Calculate the offset from the top of the page to the target section
         const offset = itemListSection.offsetTop;
-
-        // Scroll to the target section with smooth behavior
         window.scrollTo({
             top: offset,
             behavior: 'smooth'
         });
     });
 });
-
