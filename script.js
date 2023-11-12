6888e9aef8872eb9b1fb9f38d0a61eea508c15cf
Petr Boháč <bohac3@spsejecna.cz>
2023-11-12 13:57:01 +0100

    service on navbar scrools, fixed css

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-    const navLink = document.querySelector('a[aria-current="page"][href="#"]');
-    const itemListSection = document.querySelector('#header-item-list');
+    const nabidkaLink = document.querySelector('a[href="#header-item-list"]');
+    const serviceLink = document.querySelector('a[href="#service-header-list"]');
 
-    navLink.addEventListener('click', function (event) {
-        const offset = itemListSection.offsetTop;
+    nabidkaLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        const offset = document.getElementById('header-item-list').offsetTop;
         window.scrollTo({
             top: offset,
             behavior: 'smooth'
         });
+    });
+
+    serviceLink.addEventListener('click', function (event) {
+        event.preventDefault();
+        const offset = document.getElementById('service-header-list').offsetTop;
+        window.scrollTo({
+            top: offset,
+            behavior: 'smooth'
+        });
+    });
+
+    const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: '#navbar-example'
     });
 });
