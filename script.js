f16fdd9c03b1ab14b250f70800f47724509e6136
Petr Boháč <bohac3@spsejecna.cz>
2023-11-13 09:17:47 +0100

    scrollspy removed temp., nav-link clr

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ document.addEventListener('DOMContentLoaded', function () {
         target: '#navbar-example'
     });
 });
+
+window.addEventListener("scroll", function () {
+    const carousel = document.getElementById("carouselExampleIndicators");
+    const navbar = document.getElementById("navbar");
+    const navbg = document.getElementById('navbg');
+
+
+    const rect = midText.getBoundingClientRect();
+
+ 
+    if (rect.bottom < 5) {
+        navbar.classList.remove('lightnav');
+        navbar.classList.add('darknav');
+        navbg.classList.remove('lightnavbg');
+        navbg.classList.add('darknavbg');
+    } else {
+        navbar.classList.remove('darknav');
+        navbar.classList.add('lightnav');
+        navbg.classList.remove('darknavbg');
+        navbg.classList.add('lightnavbg');
+}});
